1f88a1382afae139a06587cb30c5054bf58d81f1
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-14 20:21:48 -0800

    First commit.

---
+++ Maze/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  Maze
+//
+//  Created by Jonathan Deguzman on 12/14/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *pacman;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost1;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost2;
+@property (strong, nonatomic) IBOutlet UIImageView *ghost3;
+@property (strong, nonatomic) IBOutlet UIImageView *exit;
+
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *wall;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Maze/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Maze
+//
+//  Created by Jonathan Deguzman on 12/14/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
